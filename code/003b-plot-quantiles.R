7b085b45393f40fb74e328b1421badd04c44fe5d
edrubin <edwardarubin@gmail.com>
2025-11-07 11:37:53 -0800

    fix svglite missingness

--- code/003b-plot-quantiles.R
+++ code/003b-plot-quantiles.R
@@ -16,7 +16,10 @@ args = commandArgs(trailingOnly = TRUE)
 # Setup ----------------------------------------------------------------------------------
   # Packages
   library(pacman)
-  p_load(janitor, fastverse, gtools, extrafont, fastDummies, fst, fixest, magrittr, here)
+  p_load(
+    janitor, svglite, fastverse, gtools, extrafont,
+    fastDummies, fst, fixest, magrittr, here
+  )
   fastverse_extend(topics = c('DT', 'ST', 'VI'))
   p_load(patchwork)
   # Fix 'extrafont' issue
